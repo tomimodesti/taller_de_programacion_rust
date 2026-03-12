f3ed9ee8aeac9f0dd8ecdef96b6c783cdc4a41bd
tomimodesti~ <tomimodesti@gmail.com>
2026-03-12 20:12:13 -0300

    ejercicio 2, necesita acomodar cosas pero el codigo funciona correctamente

--- src/guia1.rs
+++ src/guia1.rs
@@ -1,27 +1,9 @@
-/*Ejercicio 1: El objetivo del ejercicio es implementar un programa de consola para jugar al ahorcado.
- Bienvenido al ahorcado de FIUBA!
-///
-/// La palabra hasta el momento es: _ _ _ _ _ _
-/// Adivinaste las siguientes letras:
-/// Te quedan 5 intentos.
-Ingresa una letra: r
-
-La palabra hasta el momento es: _ _ _ _ _ r
-Adivinaste las siguientes letras: r
-Te quedan 5 intentos.
-Ingresa una letra: c
-
-Si se ingresa una letra que no forma parte de la palabra, se pierde un intento.
-
-La lista de palabras se debe leer de un archivo de texto, donde cada línea del archivo contendrá una palabra. De esa lista, se deberá elegir una palabra (puede ser una selección secuencial de palabras).
-
-El programa termina cuando se adivina correctamente la palabra pensada, o cuando se acabaron los intentos.
-*/
 
 //pensemos que recibe la palabra por consola
 //en rust se_usa_snake_case
 use std::io;
 
+#[allow(dead_code)]
 pub fn ahoracado(palabra: String, intentos: u32) -> () {
 
     let mut intentos_restantes = intentos;
@@ -83,5 +65,96 @@ fn recibir_letra() -> char {
             println!("Por favor ingresa una letra valida.");
             recibir_letra()
         } 
+    }
+}
+
+//ejercicio 2:
+/* 
+Escribir un programa para contar las frecuencias de palabras únicas leídas de un archivo de entrada. Luego imprimirlas con sus frecuencias, ordenadas primero por las más frecuentes. Por ejemplo, dado este archivo de entrada:
+
+La casa tiene una ventana
+La ventana fue defenestrada
+
+El programa debe imprimir:
+
+ventana -> 2
+La -> 2
+casa -> 1
+tiene -> 1
+una -> 1
+fue -> 1
+defenestrada -> 1
+
+Una solución básica consiste en leer el archivo línea por línea, convertirlo a minúsculas, dividir cada línea en palabras y contar las frecuencias en un HashMap. Una vez hecho esto, convertir el HashMap en una lista de pares de palabras y cantidad y ordenarlas por cantidad (el más grande primero) y por último imprimirlos.
+
+Se debe seguir las siguientes recomendaciones:
+
+    Para separar en palabras, se debe considerar los espacios en blanco, ignorando los signos de puntuación.
+    Si la frecuencia de dos palabras es la misma, no importa el orden en el que aparecen las dos palabras en la salida impresa.
+    No leer el archivo completo en memoria, se puede ir procesando línea por línea, o en conjuntos de líneas. Sí se puede mantener en memoria el hashmap completo.
+    Usar solamente las herramientas de la biblioteca std del lenguaje.
+
+Para leer un archivo línea por línea, se puede utilizar el método read_line.
+*/
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+
+pub fn contar_frecuencias(archivo: &str) -> HashMap<String,u32> {
+
+    let contenido = abrir_archivo(archivo);
+    let mut frecuencias: HashMap<String,u32> = HashMap::new();
+
+    //si no pudo abrir el archivo, devuelvo un string con el error
+    //si contenido es un error sale por el if let
+    if let Err(_e) = contenido {
+        return HashMap::from([("Error".to_string(), 0)]); // Devolvemos un HashMap con un mensaje de error
+    }
+    //si llegamos el archivo se abri correctamente,
+    //entonces leemos linea por linea, agregando los elementos al hashmap
+    let file = File::open(archivo).unwrap();
+    //buffer de lectura para leer el archivo linea por linea
+    let lector = BufReader::new(file);
+
+    for linea in lector.lines(){
+        match linea {
+            Ok(linea) => {
+                //procesamos la linea en palabras
+                let palabras = procesar_linea(&linea);
+                //actualizo el hashmap con las palabras obtenidas
+                modificar_frecuencias(&mut frecuencias, palabras);
+            }
+            Err(e) => {
+                println!("Error leyendo la linea: {}", e);
+
+            }
+        }
+    }
+    return frecuencias;
+}
+
+//funcion para abrir el archivo, devuelve un resultado, si es exitoso devuelve un unit, sino devuelve un error
+fn abrir_archivo(archivo: &str) -> std::io::Result<()> {
+    File::open(archivo)?;
+    Ok(())
+}
+
+fn procesar_linea(linea:&str) ->Vec<String> {
+    linea.split_whitespace()
+        .map(|s| quitar_signos(s))
+        .map(|s| s.to_lowercase())
+        .collect()
+}
+
+fn quitar_signos(palabra: &str) -> String {
+    palabra.chars()
+        .filter(|c| c.is_alphanumeric())
+        .collect()
+}
+
+fn modificar_frecuencias(frecuencias: &mut HashMap<String,u32>,palabras:Vec<String>) {
+    for palabra in palabras{
+        //busca la palabra en el hash, sino esta la crea con valor 0, y le suma 1
+        *frecuencias.entry(palabra).or_insert(0) += 1;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 /// Documentacion:
 // Comentario:
 
+//importo solo el modulo contar_frecuencias del archivo guia1.rs
 mod guia1;
+use guia1::contar_frecuencias;
 
 fn main() {
-    let palabra = String::from("rust");
-    let intentos = 6;
-    guia1::ahoracado(palabra, intentos);
-
+    let ruta = "archivos_ejemplo/ejemplo.txt";
+    let ruta2 = "src/guia1.rs";
+    let frecuencias = contar_frecuencias(ruta2);
+    for (palabra, frecuencia) in frecuencias.iter() {
+        println!("{}: {}", palabra, frecuencia);
+    }
 }
+
