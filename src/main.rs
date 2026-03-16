668ffe5c5e27a700f3fd3ce2af71ea7d7f1f440a
tomimodesti~ <tomimodesti@gmail.com>
2026-03-16 14:52:57 -0300

    avance en tp1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 /// Documentacion:
 // Comentario:
 
-//importo solo el modulo contar_frecuencias del archivo guia1.rs
-mod guia1;
-use guia1::contar_frecuencias;
+mod minikv;
+use minikv::main_tp;
 
 fn main() {
-    let ruta = "archivos_ejemplo/ejemplo.txt";
-    let ruta2 = "src/guia1.rs";
-    let frecuencias = contar_frecuencias(ruta2);
-    for (palabra, frecuencia) in frecuencias.iter() {
-        println!("{}: {}", palabra, frecuencia);
-    }
+    main_tp();
 }
 
