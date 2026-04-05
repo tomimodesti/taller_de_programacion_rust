cc566bdcaa6d608d1f454928acdd30f97deb04d2
tomimodesti~ <tomimodesti@gmail.com>
2026-04-05 15:17:00 -0300

    agrego enum mensajes de persistencia

--- src/minikv/estructuras.rs
+++ src/minikv/estructuras.rs
@@ -3,3 +3,9 @@
 use std::io::{Read, Seek, Write};
 pub trait Storage: Read + Write + Seek + Send {}
 impl<T: Read + Write + Seek + Send> Storage for T {}
+
+pub enum MensajePersistencia {
+    Snapshot,
+    Set { clave: String, valor: String },
+    Delete { clave: String },
+}
