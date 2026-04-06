faaf0c423b6448e5043ceb8605636403666c0da2
tomimodesti~ <tomimodesti@gmail.com>
2026-04-05 23:30:48 -0300

    agrego nuevos errores al enum

--- src/minikv/errores.rs
+++ src/minikv/errores.rs
@@ -11,15 +11,36 @@ pub enum KvErrores {
     ConnectionClosed,
     Timeout,
     ClientSocketBinding,
+    InvalidArgs,
+    InvalidPuerto,
+    EOF,
+    EMPTY,
+    //error default, errores que son especificos
+    Error(String)
 }
 
 impl KvErrores {
     pub fn es_recuperable(&self) -> bool {
+        matches!(
+            self,
+            Self::NotFound | Self::ExtraArgument | Self::MissingArgument | Self::UnknownCommand
+        )
+    }
+    pub fn to_str(&self) -> String {
         match self {
-            Self::NotFound | Self::ExtraArgument | Self::MissingArgument | Self::UnknownCommand => {
-                true
-            }
-            _ => false,
-        }
+        Self::NotFound => "Not Found".to_string(),
+        Self::ExtraArgument => "Extra Argument".to_string(),
+        Self::MissingArgument => "Missing Argument".to_string(),
+        Self::UnknownCommand => "Unknown Command".to_string(),
+        Self::IoError(_) => "IoError".to_string(),
+        Self::ConnectionClosed => "Connection Closed".to_string(),
+        Self::Timeout => "Timeout".to_string(),
+        Self::ClientSocketBinding => "Client Socket Binding".to_string(),
+        Self::InvalidArgs => "Invalid Args".to_string(),
+        Self::InvalidPuerto => "Invalid Puerto".to_string(),
+        Self::EOF => "EOF".to_string(),
+        Self::EMPTY => "EMPTY".to_string(),
+        Self::Error(mensaje) => mensaje.clone(),
+    }
     }
 }
