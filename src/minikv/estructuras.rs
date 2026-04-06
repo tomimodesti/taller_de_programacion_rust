55a31220dd3aa181b06b833cb1402c16ae6e18c4
tomimodesti~ <tomimodesti@gmail.com>
2026-04-06 11:39:26 -0300

    documentacion

--- src/minikv/estructuras.rs
+++ src/minikv/estructuras.rs
@@ -1,4 +1,4 @@
-//archivo para traits y estructuras comunes a todo el proyecto
+//! archivo para traits y estructuras comunes a todo el proyecto
 
 use std::io::{Read, Seek, Write};
 pub trait Storage: Read + Write + Seek + Send {}
